bbe297efe9cdf0db10e58e9d3f593a886dbd5985
Vinishireis <nishimuravinicius28@gmail.com>
2025-09-22 20:04:14 -0300

    Atualização de Pastas e Organização de Arquivos

--- app/src/main/java/com/example/aplicativocomedoriadatia/model/Product.java
+++
@@ -1,9 +0,0 @@
-package com.example.aplicativocomedoriadatia.model;
-
-public class Product {
-    public int id;
-    public String name;
-    public String image_url;
-    public double price;
-    public String category;
-}
