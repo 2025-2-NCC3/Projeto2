1eff742d9e22bdd78940acf932e1696630495149
unknown <you@example.com>
2025-10-26 05:04:39 -0300

    Popup - prototipo

---
+++ app/src/main/java/com/example/popupderetirada/PopupRetiradaActivity.java
@@ -0,0 +1,34 @@
+package com.example.popupderetirada;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PopupRetiradaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popup_retirada);
+        mostrarPopupRetirada();
+    }
+
+    private void mostrarPopupRetirada() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("📦 Produto Pronto")
+                .setMessage("Seu pedido está disponível para retirada no balcão.")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(PopupRetiradaActivity.this, "Obrigado!", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+
+                    }
+                })
+                .setCancelable(false)
+                .show();
+    }
+}
